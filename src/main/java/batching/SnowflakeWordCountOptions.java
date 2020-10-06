76c6058192f2c01aa2f63d6b7ed5caba595f986c
pawel.urbanowicz <pawel.urbanowicz@polidea.com>
2020-10-06 15:16:04 +0200

    feat: add KMS support

--- src/main/java/batching/SnowflakeWordCountOptions.java
+++ src/main/java/batching/SnowflakeWordCountOptions.java
@@ -4,6 +4,7 @@ import org.apache.beam.sdk.io.snowflake.SnowflakePipelineOptions;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.Validation.Required;
+import org.apache.beam.sdk.options.ValueProvider;
 
 /**
  * Supported PipelineOptions used in provided examples.
@@ -21,4 +22,10 @@ public interface SnowflakeWordCountOptions extends SnowflakePipelineOptions {
     String getOutput();
 
     void setOutput(String value);
+
+    @Description(
+            "KMS Encryption Key should be in the format projects/{gcp_project}/locations/{key_region}/keyRings/{key_ring}/cryptoKeys/{kms_key_name}")
+    ValueProvider<String> getKMSEncryptionKey();
+
+    void setKMSEncryptionKey(ValueProvider<String> keyName);
 }
